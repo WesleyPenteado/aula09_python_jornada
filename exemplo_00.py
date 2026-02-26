a0a436c65f87e70d378d4a09007b6ebf85defd4c
WesleyPenteado <wesleycosta.ne@gmail.com>
2026-02-26 13:04:51 -0800

    Atualizacao aula de logger

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,4 +1,17 @@
-def soma(x, Y):
-    return x + Y
+from loguru import logger
+from sys import stderr
+
+
+logger.add("meu_log.log", level="CRITICAL")
+
+def soma(x, y):
+    try:
+        soma = x + y
+        logger.info(f'voce digitou valores corretos, parabens {soma}')
+        return soma
+    except:
+        logger.critical("Voce precisa digitar valores corretos")
 
 print(soma(2,3))
+print(soma(2,7))
+print(soma(2,"3"))
